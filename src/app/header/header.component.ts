6abdea13442c599b9a61d3e6a7c4cc438b36ff03
unknown <ben.garrison01@gmail.com>
2018-05-01 20:42:31 -0600

    First Commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['../app.component.css']
+})
+
+export class HeaderComponent {
+
+}
